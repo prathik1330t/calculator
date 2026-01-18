24ed6506fc621a4a3ea0ab4d502633b2c852dacc
prathik1330t <01fe24bca234@kletech.ac.in>
2026-01-18 11:58:59 +0530

    Add files via upload

---
+++ test_calculator.py
@@ -0,0 +1,13 @@
+from calculator import sum
+
+def add():
+    assert sum(10,5)==15
+
+def sub():
+    assert sum(10,5)==5
+
+def mul():
+    assert sum(10,5)==50
+
+def div():
+    assert sum(10,5)==2
